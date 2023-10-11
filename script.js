1c05c547adf5b87235f561eb774e1da7fef36e9d
vashukashyap <kashyapvashu2003@gmail.com>
2023-10-12 01:36:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+API_KEY = '8162de3d5c4c1983420f878abcaf2f0a'
+URL = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q='
+
+const searchBox = document.querySelector('.search input')
+const searchBtn = document.querySelector('.search Button')
+
+
+const checkWeather = async (city) => {
+    const response = await fetch(URL + city + `&appid=${API_KEY}`);
+
+    if (response.status == 404){
+        document.querySelector('.error').style.display = 'block';
+        document.querySelector('.weather').style.display = 'none';
+    }
+    let data = await response.json();
+    let iconcode = data.weather[0].icon;
+    let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+
+    console.log(data);
+
+
+    if(document.querySelector('.error').style.display != 'none'){
+    document.querySelector('.error').style.display = 'none';
+    }
+    document.querySelector('.weather-icon').src = iconurl
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.temp').innerHTML = data.main.temp + '°C';
+    document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
+    document.querySelector('.wind').innerHTML = data.wind.speed + ' km/hr';
+    document.querySelector('.weather').style.display = 'block';
+
+        
+}
+
+
+
+
+searchBtn.addEventListener('click', ()=>{
+    checkWeather(searchBox.value);
+})
+
+
+
+// checkWeather()
+
+
+
